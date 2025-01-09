bad017a8477c737aac643538e74433a15c38b8f8
Codein <119778954+Cod331n@users.noreply.github.com>
2025-01-09 22:00:01 +0300

    Update start_with_auto_enabling.sh

--- start_with_auto_enabling.sh
+++ start_with_auto_enabling.sh
@@ -1,29 +1,19 @@
 # use for situations, when you need to automatically enable your server to be started if it was stopped.
 # this shell file should be in the same dir with server jar file, or you can cd to it by your own
 
-# checking interval of server is enabled or not
-CHECK_INTERVAL=10
-
-is_server_running() {
-    if screen -list | grep -q "minecraft"; then
-        return 0
-    else
-        return 1
-    fi
-}
+# delay before enabling
+ENABLE_DELAY=10
 
 # use start.sh from
 # https://github.com/Cod331n/mc-server-shell/blob/main/start.sh
 start_server() {
     echo "Server was stopped. Enabling..."
     chmod +x start.sh
-    # it's necessary to have permissions to start this file
     ./start.sh
 }
 
 while true; do
-    if ! is_server_running; then
-        start_server
-    fi
-    sleep $CHECK_INTERVAL
+    start_server
+    echo "Server has been stopped. Enabling..."
+    sleep $ENABLE_DELAY
 done
