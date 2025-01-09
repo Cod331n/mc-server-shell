f6943b35b9b18104383e6a1aefe516c8300e8e6f
Codein <119778954+Cod331n@users.noreply.github.com>
2025-01-09 22:48:35 +0300

    Create start_with_restarting.sh

---
+++ start_with_restarting.sh
@@ -0,0 +1,31 @@
+# restart time period in seconds
+RESTART_PERIOD=3600
+
+is_server_running() {
+    if screen -list | grep -q minecraft; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+start_server() {
+    echo "Enabling server..."
+    chmod +x start.sh
+    screen -dmS minecraft ./start.sh 
+}
+
+stop_server() {
+    screen -S minecraft -X stuff "stop\n"
+    sleep 5
+    screen -S minecraft -X quit
+}
+
+while true; do
+    if ! is_server_running; then
+        start_server
+    fi
+    
+    sleep $RESTART_PERIOD
+    stop_server
+done
