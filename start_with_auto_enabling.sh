42684522a09f12909c4270ba60085fd1932acfda
Codein <119778954+Cod331n@users.noreply.github.com>
2025-01-09 22:00:47 +0300

    Update start_with_auto_enabling.sh

--- start_with_auto_enabling.sh
+++ start_with_auto_enabling.sh
@@ -7,13 +7,13 @@ ENABLE_DELAY=10
 # use start.sh from
 # https://github.com/Cod331n/mc-server-shell/blob/main/start.sh
 start_server() {
-    echo "Server was stopped. Enabling..."
+    echo "Enabling server..."
     chmod +x start.sh
     ./start.sh
 }
 
 while true; do
     start_server
-    echo "Server has been stopped. Enabling..."
+    echo "Server has been stopped."
     sleep $ENABLE_DELAY
 done
